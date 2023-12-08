312f42524d4270fe6573e0ec7935eb56a1d03331
Abby Volk <abigail_volk@partner.nps.gov>
2023-12-08 13:13:51 -0700

    working graph summarizing the trend slopes by discharge statistic and rcp

--- projection_summary_analysis.R
+++ projection_summary_analysis.R
@@ -5,23 +5,56 @@ packages <-  c("tidyverse") #create a list of all required packages
 lapply(packages, library, character.only = T)
 
 
-#### Read in Projection Results Table ####
+#### Read in and Wrangle Projection Results Table ####
 projection_results <- read_csv("v5_EGRETprojectionresults.csv") %>% 
-  separate(model, into = c("model", "rcp"), sep = "_") # create an RCP column
+  separate(model, into = c("model", "rcp"), sep = "_") %>%  # create an RCP column
+  filter(model != "MIROC-ESM-CHEM")
+# Let's move the peak flow stuff to a different data frame
+projection_peak_flow <- projection_results %>% 
+  filter(discharge_stat == "peakflow_TS" | discharge_stat == "peakflow_OLS")
 
-# Join the istats
-istat_table <- matrix(c("1", "Annual minimum 1-day daily mean discharge", 
-                        "2", "Annual minimum 7-day mean of the daily mean discharges", 
-                        "3", "Annual minimum 30-day mean of the daily mean discharges",
-                        "4", "Annual median of the daily mean discharges", 
-                        "5", "Annual mean of the daily mean discharges",
-                        "6", "Annual maximum 30-day mean of the daily mean discharges", 
-                        "7", "Anuual maximum 7-day mean of the daily mean discharges",
-                        "8", "Annual maximum 1-day daily mean discharges"), nrow = 8, ncol = 2, byrow = TRUE, dimnames = list(NULL, c("discharge_stat", "Discharge Statistic Name")))
+projection_istats <- projection_results %>% filter(!grepl("peakflow", discharge_stat))
+
+# Join the istats to the table
+istat_table <- matrix(c("1", "Minimum 1-day", 
+                        "2", "Minimum 7-day mean", 
+                        "3", "Minimum 30-day mean",
+                        "4", "Median", 
+                        "5", "Mean",
+                        "6", "Maximum 30-day mean", 
+                        "7", "Maximum 7-day mean",
+                        "8", "Maximum 1-day"), nrow = 8, ncol = 2, byrow = TRUE, dimnames = list(NULL, c("discharge_stat", "Discharge Statistic Name")))
 istat_table <- as.data.frame(istat_table)
-projection_results <- left_join(projection_results, istat_table, by = "discharge_stat")
+projection_istats <- left_join(projection_istats, istat_table, by = "discharge_stat")
 
 
+#### Visualize the Slopes ####
+# Istats trend slopes
+
+# change in cfs per year
+projection_istats %>% 
+  filter(model != "Historical") %>% 
+  ggplot(aes(x=`Discharge Statistic Name`, y = slope, fill=rcp)) + 
+  geom_boxplot(position = "dodge") +
+  geom_hline(yintercept = 0) +
+  labs(y = "Theil-Sen Trend Slope: cfs/year (2023-2100)") +
+  coord_flip() +
+  theme_bw()
+
+# or % change per year
+projection_istats %>% 
+  filter(model != "Historical") %>% 
+  ggplot(aes(x=`Discharge Statistic Name`, y = slopepct_mag, fill=rcp)) + 
+  geom_boxplot(position = "dodge") +
+  geom_hline(yintercept = 0) +
+  labs(y = "Theil-Sen Trend Slope: percent change per year (2023-2100)") +
+  coord_flip() +
+  theme_bw()
+
+
+
+
+#### If you want to summarize the slopes (avg, for example) ####
 slope_mean <- projection_results %>% 
   filter(model != "Historical") %>% 
   group_by(discharge_stat, rcp, `Discharge Statistic Name`) %>% 
@@ -34,10 +67,3 @@ slopepct_mag_mean <- projection_results %>%
   summarise(slopepct_mag = mean(slopepct_mag),
             slopepct_mag_sd = sd(slopepct_mag))
 
-
-
-slope_median <- projection_results %>% 
-  filter(model != "Historical") %>% 
-  group_by(discharge_stat, rcp) %>% 
-  summarise(slope_median = median(slope),
-            slope_sd = sd(slope))
