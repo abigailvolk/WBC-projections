53acb025e179c54fd248a1b8d6e260eedec914cf
Abby Volk <abigail_volk@partner.nps.gov>
2023-12-15 15:23:37 -0700

    nightly commit: not done with merging historical measured with projected, need to finish seasonal graphs with different time periods

--- projected_seasonal.R
+++ projected_seasonal.R
@@ -1,8 +1,38 @@
+#### Read in libraries and NPS font #### --------------------------------------
+
 library(tidyverse)
 library(lubridate)
+library(EGRET)
+
+windowsFonts("Frutiger LT Std 55 Roman" = 
+               windowsFont("Frutiger LT Std 55 Roman"))
+
+fontsize <- 20
+nps_font <- "Frutiger LT Std 55 Roman"
+nps_theme2 <- function(base_size = fontsize, 
+                       base_family=nps_font,
+                       title = T) {
+  theme_classic(base_size = base_size, base_family = nps_font) %+replace%
+    theme(axis.text.x = element_text(family=nps_font, size = base_size * 0.8),
+          complete = TRUE
+    ) 
+}
+
+
+#### Read in Projections csv and historical measured #### -------------------
 
 projections <- read_csv("daily_df.csv") # read in daily with all models
+Daily <- readNWISDaily("09505200","00060", "", "2022-10-01")
+test <- as_tibble(Daily) %>% 
+  mutate(cfs = Q*35.314666212661) %>% 
+  select()
+
+
+#### Wrangle Projections #### -------------------------------------------------
+
+
 projections$daily_cfs <- projections$total*35.31467*(287.4887/86.4) # mm to cfs
+
 # mm to cfs for WBC = 117.5066, cfs to mm = 0.00851016
 # get rid of the Miroc gcm, mutate the model string.
 projections <- projections %>% 
@@ -14,36 +44,26 @@ projections <- projections %>%
   rename(Year = yr,
          cfs = daily_cfs)
 
-cfs_historical <- projections %>% select(Day, cfs, Year, rcp)
-cfs_historical <- cfs_historical %>% 
+cfs_historical <- projections %>% 
+  mutate(time_period = case_when(Year <= 2022 ~ 'low',
+                            points < 25 ~ 'med',
+                            points < 35 ~ 'high'))
+
   group_by(Day, rcp) %>% 
   summarize(mean = mean(cfs),
             median = median(cfs)) 
 
-cfs_historical %>% ggplot(aes(x=Day, color=rcp)) +
-  #geom_line(aes(y=mean), lwd=1) +
-  geom_smooth(aes(y=mean), se=F, span=0.1) +
-  theme_bw() + 
-  labs(x="Days Since January 1st", y="Discharge (cfs)") +
-  theme(legend.title=element_blank()) +
-  scale_color_brewer(c("black", "yellow", "red"))
+  
+  
+#### Plotting Hydrographs ####  
   
 
-
-
-cfs_historical <- pivot_wider(cfs_historical, names_from = Day, values_from = cfs)
-quants <- c(0.25,0.50, 0.75)
-Percentiles <- apply(cfs_historical[2:dim(cfs_historical)[2]], 2, quantile, probs = quants, na.rm = TRUE)
-Mean <- apply(cfs_historical[2:dim(cfs_historical)[2]], 2, mean, na.rm = TRUE)
-historical_stats <- t(rbind(Percentiles, Mean))
-historical_stats <- as.data.frame(historical_stats)
-historical_stats$Day <- c(1:366)
-
-historical_stats %>% ggplot(aes(x=Day)) +
-  geom_line(aes(y=`50%`, color="Median"), lwd=1) +
-  geom_line(aes(y=Mean, color="Mean"), lwd=1) +
+cfs_historical %>% ggplot(aes(x=Day, color=rcp)) +
+  geom_smooth(aes(y=mean), se=F, span=0.2) +
   theme_bw() + 
   labs(x="Days Since January 1st", y="Discharge (cfs)") +
-  nps_theme2() +
   theme(legend.title=element_blank()) +
-  scale_color_manual(values = c("Mean" = "orange", "Median" = "black"))
+  scale_color_manual(values = c("orange","red",  "black")) +
+  nps_theme2()
+  
+
