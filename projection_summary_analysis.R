eb1eac373e4e2c5ad6f7493b6a4e83ea04558138
Abby Volk <abigail_volk@partner.nps.gov>
2023-12-29 13:26:02 -0700

    Added monsoon peak flow figures to projection_summary_analysis. Working on code that saves them to file

--- projection_summary_analysis.R
+++ projection_summary_analysis.R
@@ -13,20 +13,22 @@ nps_theme2 <- function(base_size = fontsize, base_family=nps_font) {
           complete = TRUE
     )}
 
+save_path <- file.path("projection_summary_analysis_output")
 #### Read in and Wrangle Projection Results Table ####
 projection_results <- read_csv("v8_EGRETprojectionresults.csv") %>% 
-  separate(model, into = c("model", "rcp"), sep = "_") %>%  # create an RCP column
-  filter(model != "MIROC-ESM-CHEM")
+  separate(model, into = c("model", "rcp"), sep = "_")
 
 # Move the peak flow stuff to a different data frame
 projection_peak_flow <- projection_results %>% 
-  filter(discharge_stat == "peakflow_TS" | discharge_stat == "peakflow_OLS")
+  filter(grepl("peakflow", discharge_stat) | 
+           grepl("waterYear", discharge_stat))  %>% 
+  mutate(change_over_record = slope*77) # find change over the record
 
 projection_istats <- projection_results %>% 
-  filter(!grepl("peakflow", discharge_stat)) %>% 
-  mutate(cfs_change_over_record = slope*77)
+  filter(!grepl("peakflow", discharge_stat) & !grepl("waterYear", discharge_stat)) %>% 
+  mutate(cfs_change_over_record = slope*77) # find change over the record
 
-# Join the istats to the table
+# Join the istats and regressions to the table
 istat_table <- matrix(c("1", "Minimum 1-day", 
                         "2", "Minimum 7-day mean", 
                         "3", "Minimum 30-day mean",
@@ -34,62 +36,103 @@ istat_table <- matrix(c("1", "Minimum 1-day",
                         "5", "Mean",
                         "6", "Maximum 30-day mean", 
                         "7", "Maximum 7-day mean",
-                        "8", "Maximum 1-day"), nrow = 8, ncol = 2, byrow = TRUE, dimnames = list(NULL, c("discharge_stat", "Discharge Statistic Name")))
+                        "8", "Maximum 1-day"), nrow = 8, ncol = 2, byrow = TRUE, 
+                      dimnames = list(NULL, 
+                                      c("discharge_stat", "Discharge Statistic Name")))
 istat_table <- as.data.frame(istat_table)
 projection_istats <- left_join(projection_istats, istat_table, by = "discharge_stat")
 
+regressions_table <- tribble(
+  ~discharge_stat, ~`Discharge Statistic Name`,
+  "peakflow_TS", "Theil-Sen Peak Flow Day ~ Water Year",
+  "peakflow_OLS", "OLS Peak Flow Day ~ Water Year",
+  "Monsoon , wateryear_day ~ waterYear", "Monsoon",
+  "Monsoon , cfs ~ waterYear", "Monsoon",
+  "Other , wateryear_day ~ waterYear", "Non-Monsoon",
+  "Other , cfs ~ waterYear", "Non-Monsoon",
+  
+)
+projection_peak_flow <- left_join(projection_peak_flow, regressions_table, 
+                               by = "discharge_stat")
 
 #### Visualize the Slopes ####
-# Istats trend slopes
+##### Istats trend slopes
 
-# change in cfs per year
-projection_istats %>% 
-  filter(model != "Historical") %>% 
-  ggplot(aes(x=`Discharge Statistic Name`, y = slope, fill=rcp)) + 
-  geom_boxplot(position = "dodge") +
-  geom_hline(yintercept = 0) +
-  labs(y = "Theil-Sen Trend Slope: cfs/year (2023-2100)") +
-  coord_flip() +
-  theme_bw() +
-  scale_fill_manual(values = c("orange", "red")) +
-  nps_theme2()
+# change in CFS PER YEAR
+# projection_istats %>% 
+#   filter(model != "Historical") %>% 
+#   ggplot(aes(x=`Discharge Statistic Name`, y = slope, fill=rcp)) + 
+#   geom_boxplot(position = "dodge") +
+#   geom_hline(yintercept = 0) +
+#   labs(x= "Discharge Statistic",
+#        y = "Theil-Sen Trend Slope: cfs/year (2023-2100)") +
+#   coord_flip() +
+#   theme_bw() +
+#   scale_fill_manual(values = c("orange", "red")) +
+#   nps_theme2()
 
-# or change over the record
+# or CHANGE OVER RECORD 
 projection_istats %>% 
   filter(model != "Historical") %>% 
   ggplot(aes(x=`Discharge Statistic Name`, y = cfs_change_over_record, fill=rcp)) + 
   geom_boxplot(position = "dodge") +
   geom_hline(yintercept = 0) +
-  labs(y = "Cfs change over record (2023-2100)") +
+  labs(x= "Discharge Statistic",
+       y = "Cfs change over record (2023-2100)") +
   coord_flip() +
   theme_bw() +
   scale_fill_manual(values = c("orange", "red")) +
   nps_theme2()
 
 
-# or % change per year
-projection_istats %>% 
-  filter(model != "Historical") %>% 
-  ggplot(aes(x=`Discharge Statistic Name`, y = slopepct_mag, fill=rcp)) + 
+# # or % change per year
+# projection_istats %>% 
+#   filter(model != "Historical") %>% 
+#   ggplot(aes(x=`Discharge Statistic Name`, y = slopepct_mag, fill=rcp)) + 
+#   geom_boxplot(position = "dodge") +
+#   geom_hline(yintercept = 0) +
+#   labs(y = "Theil-Sen Trend Slope: percent change per year (2023-2100)") +
+#   coord_flip() +
+#   theme_bw()
+
+
+###### Peak flow slopes-------------------------------------------
+
+# Boxplot: Change in DAY of peak flow over record by season (days/yr*77)
+projection_peak_flow %>% 
+  filter(grepl("wateryear_day", discharge_stat) &
+           model != "Historical") %>% 
+  ggplot(aes(x= `Discharge Statistic Name`, y = change_over_record, fill=rcp)) + 
   geom_boxplot(position = "dodge") +
   geom_hline(yintercept = 0) +
-  labs(y = "Theil-Sen Trend Slope: percent change per year (2023-2100)") +
+  labs(x = "Season",
+       y = "Change in day of peak flow over record (2023-2100)") +
   coord_flip() +
-  theme_bw()
+  theme_bw() +
+  scale_fill_manual(values = c("orange", "red")) +
+  nps_theme2()
+ggsave("Figure 3-5 production adjrsqs_reclass2.pdf", 
+       device = cairo_pdf, dpi=300, 
+       width = 12, height = 9, units="in")
+
+# Boxplot: Change in magnitude of peak flow over record by season (cfs/yr*77)
+projection_peak_flow %>% 
+  filter(grepl("cfs", discharge_stat) &
+           model != "Historical") %>% 
+  ggplot(aes(x= `Discharge Statistic Name`, y = change_over_record, fill=rcp)) + 
+  geom_boxplot(position = "dodge") +
+  geom_hline(yintercept = 0) +
+  labs(x = "Season",
+       y = "Change in magnitude of peak flow (cfs) over record (2023-2100)") +
+  coord_flip() +
+  theme_bw() +
+  scale_fill_manual(values = c("orange", "red")) +
+  nps_theme2()
+ggsave("Figure 3-5 production adjrsqs_reclass2.pdf", 
+       device = cairo_pdf, dpi=300, 
+       width = 12, height = 9, units="in")
 
 
 
 
-#### If you want to summarize the slopes (avg, for example) ####
-slope_mean <- projection_results %>% 
-  filter(model != "Historical") %>% 
-  group_by(discharge_stat, rcp, `Discharge Statistic Name`) %>% 
-  summarise(slope_mean = mean(slope),
-            slope_sd = sd(slope))
-
-slopepct_mag_mean <- projection_results %>% 
-  filter(model != "Historical") %>% 
-  group_by(discharge_stat, rcp) %>% 
-  summarise(slopepct_mag = mean(slopepct_mag),
-            slopepct_mag_sd = sd(slopepct_mag))
 
